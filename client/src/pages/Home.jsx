cff8f1d0858be25cc8198d0bdeade32b1890bb3a
Samir <ssardarlisamir@gmail.com>
2023-02-02 00:16:02 +0400

    button text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -81,7 +81,7 @@ function Home() {
                             setForModal("Add")
                             setValues({ text: "" })
                         }}>
-                            Add Product
+                            Add Todo
                         </button>
                     </div>
 
